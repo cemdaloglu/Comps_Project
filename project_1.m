4e6195dbab9172470e2b5c1a91c839ec6402f6ff
Cem Daloglu <chemdaloglu@gmail.com>
2022-08-13 00:46:36 +0200

    Update project_1.m

--- project_1.m
+++ project_1.m
@@ -54,18 +54,43 @@ support_saver = zeros(m, m);
 for idx = 1:m
     support=[];
     res=b_random_arr(:, idx);
-    while norm(res, 2) > 10^(-6)
+    count = 0;
+    while norm(res, 2) > 10^(-6) && count < m
         cor = abs(A_random' * res);
         [a, b] = max(cor);
         support = [support b];
+        A_sup = zeros(m, n);
+        A_sup(:, support) = A_random(:, support);
 
         cvx_begin
-            variable x_hat3(size(support, 2), 1)
-            minimize(norm(b_random_arr(:, idx) - A_random(:,support) * x_hat3, 2))
+            variable x_hat3(n, 1)
+            minimize(norm(b_random_arr(:, idx) - A_sup * x_hat3, 2))
+            subject to
+            x_hat3(support, :) ~= 0;
         cvx_end
 
-        res = b_random_arr(:, idx) - A_random(:,support) * x_hat3;
+        res = b_random_arr(:, idx) - A_sup * x_hat3;
+        count = count + 1;
     end
-    x_hat3_saver(1:size(support, 2), idx) = x_hat3;
-    support_saver(idx, 1:size(support, 2)) = support;
+    x_hat3_saver(:, idx) = x_hat3;
+    support_saver(idx, :) = support;
 end
+%% Matching Pursuit
+x_hat4_saver = zeros(n, m);
+
+for idx = 1:m
+    res = b_random_arr(:, idx);
+    count = 0;
+    x_hat_4 = zeros(n, 1);
+    while norm(res, 2) > 10^(-6) && count < m
+        cor = abs(A_random' * res);
+        [a, b] = max(cor);
+        A_sup = A_random(:, b);
+        t = A_sup' * res;
+        x_hat_4(b, :) = x_hat_4(b, :) + t;
+        res = res - t * A_sup;
+        count = count + 1;
+    end
+    x_hat4_saver(:, idx) = x_hat4;
+end
+%% Iterative Hard Thresholding
